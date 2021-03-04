8fc348a19cba61221100bf79b24c5c26801dfe11
Montana <montana@Montanas-MacBook-Pro.local>
2021-03-03 23:47:21 -0500

    UI screen with auto layout constraints

---
+++ L04ExerciseSolution/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  L04ExerciseSolution
+//
+//  Created by Montana  on 3/1/21.
+//  Copyright © 2021 Montana Braswell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
